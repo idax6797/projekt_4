19827e8ee8006bfaf1aa5521931b4cfa8f03f046
bene2510 <bene2510@edu.ucl.dk>
2020-09-22 19:30:47 +0200

    Billedkarrusel virker finally
    Forhåbentlig...

--- post_3/post_3.js
+++ post_3/post_3.js
@@ -1,5 +1,32 @@
 console.log("Hello World");
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides1", "mySlides2");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
+
+//halllooooo hvad sker der lige altså... fatter hat
+
 var slideshow1 = document.getElementById("slideshow1");
 slideshow1.currentSlideIndex = 1;
 showSlides(slideshow1.currentSlideIndex, slideshow1);
@@ -22,19 +49,19 @@ function showSlides(n, slideshow) {
 
 
     var i;
-    var slides = slideshow.getElementsByClassName("slideshow");
-    var dots = slideshow.getElementsByClassName("dot");
+    var slides = slideshow.getElementsByClassName("mySlides1");
+    //men men men... har kun mySlides1, mySlides2 og mySlides3 som classes...
     if (n > slides.length) { slideshow.currentSlideIndex = 1 }
     if (n < 1) { slideshow.currentSlideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+
     slides[slideshow.currentSlideIndex - 1].style.display = "block";
-    dots[slideshow.currentSlideIndex - 1].className += " active";
+
 }
+
+
 
 
 
