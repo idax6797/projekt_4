bcb754ee30b35fd920a83f9fe1642549de1a2be9
juliekinch <juliekinch@hotmail.com>
2020-09-23 15:11:14 +0200

    quizzzzzzz

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -8,9 +8,9 @@
 //   }
 
 function forkert_svar() {
-    window.location.href = "wrong.html";
+    window.location.href = "../wrong/wrong.html";
 }
 
 function korrekt_svar() {
-    window.location.href = "correct.html";
+    window.location.href = "../correct/correct.html";
 }
