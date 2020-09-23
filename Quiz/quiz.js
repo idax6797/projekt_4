4fe2f8ca8c43aec9aa0c1e81e0db1fed410ff117
juliekinch <juliekinch@hotmail.com>
2020-09-23 15:02:25 +0200

    quizzzzz

---
+++ Quiz/quiz.js
@@ -0,0 +1,16 @@
+
+// function korrekt_svar() {
+//     document.getElementById("result").innerHTML = "Korrekt!";
+//   }
+
+//   function forkert_svar() {
+//     document.getElementById("result").innerHTML = "Forkert!";
+//   }
+
+function forkert_svar() {
+    window.location.href = "wrong.html";
+}
+
+function korrekt_svar() {
+    window.location.href = "correct.html";
+}
